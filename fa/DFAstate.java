8023ab986bd6dc8639a170634bd1bcb2ea30046d
amylee78 <142922298+amylee78@users.noreply.github.com>
2026-02-09 16:15:14 -0700

    DFAState wip, added in constructor, add and get Transition

--- fa/DFAstate.java
+++ fa/DFAstate.java
@@ -1,5 +1,23 @@
 package fa;
 
-public class DFAstate extends State {
-    //to be written
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class DFAState extends State {
+
+
+    private Map<Character, DFAState> transitions;
+
+    public DFAState(String name) {
+        super(name);
+        transitions = new LinkedHashMap<>();
+    }
+
+    public void addTransition(char symbol, DFAState toState) {
+        transitions.put(symbol, toState);
+    }
+
+    public DFAState getTransition(char symbol) {
+        return transitions.get(symbol);
+    }
 }
